7791068a385832c1a5e1b8b18da59ac1ce058540
fenixinvitado2021 <104691589+fenixinvitado2021@users.noreply.github.com>
2022-07-02 16:24:08 -0400

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -325,9 +325,9 @@ async def youtube_dl_call_back(bot, update):
             media_album_p = []
             if images is not None:
                 i = 0
-                caption = "© @xTeamBots"
+                caption = "-£> @raydel0307"
                 if is_w_f:
-                    caption = "@xurluploaderbot"
+                    caption = "URL UPLOADER"
                 for image in images:
                     if os.path.exists(str(image)):
                         if i == 0:
